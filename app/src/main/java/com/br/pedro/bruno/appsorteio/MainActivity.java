2360f6cba2d6577f84413eee89dd992a8d0bd0e9
Pedro Bruno <pbruno1283@gmail.com>
2021-06-18 10:25:06 -0300

    first commit

---
+++ app/src/main/java/com/br/pedro/bruno/appsorteio/MainActivity.java
@@ -0,0 +1,28 @@
+package com.br.pedro.bruno.appsorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        TextView txtValorSort = findViewById(R.id.txtValorSort);
+
+        int numero = new Random().nextInt(11);
+
+
+        txtValorSort.setText("Número: "+Integer.toString(numero));
+    }
+}
